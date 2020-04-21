2d9cd33b9e91ad92977705f2f35b189d562d2c3a
emE_Brou <57578552+EmePin@users.noreply.github.com>
2020-04-20 19:46:25 -0500

    Update Digitos.java

--- Digitos.java
+++ Digitos.java
@@ -1,5 +1,5 @@
 /**
- * @Descripción: Realizar un programa que contenga un 
+ * Descripción: Realizar un programa que contenga un 
  * método que su parámetro sea una cadena de números enteros, 
  * y que realice la suma de todos los números de un solo dígito que se puedan 
  * extraer del número dado, la suma ahora será el número a abstraer dígitos, 
