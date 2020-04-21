d169775cd7fbf46d4b16dd81a4312d7a3d40dd17
emE_Brou <57578552+EmePin@users.noreply.github.com>
2020-04-20 19:45:53 -0500

    Update Digitos.java

--- Digitos.java
+++ Digitos.java
@@ -1,5 +1,3 @@
-
-
 /**
  * @Descripción: Realizar un programa que contenga un 
  * método que su parámetro sea una cadena de números enteros, 
@@ -13,20 +11,30 @@
 public class Digitos
 {// se crea la clase Dígitos
     // se declaran las variables de instancia
+    
+    /**
+     * Campos de clase
+       */
     private String numero;
     private int suma;
-    
+    /**
+     * Constuctor para la clase Digitos
+       */
     public Digitos()// se ejecuta el método constructor Dígitos
     {
         // se inicializan las variables 
         numero = " ";
         suma = 0;
     }
-
+    /**
+     
+     
+     *@param numero El parametro numero define el numero que va a acceder el usuario
+     */
     public void SumaDigitos(String numero)
     {// inicio del método que realiza la suma de los dígitos del número a ingresar
         do{// Inicio del do - while
-            int longitud = numero.length();/// se obtiene el valor de longitud con el método .length() del número que nos dieron 
+            int longitud = numero.length();/// se obtiene el valor de la variable local longitud con el método .length() del número que nos dieron 
             suma = 0;//cuando siguen quedando dos dígitos en la pprimera suma se tine que iniciar de cero otra vez
             for (int i = 0; i < longitud; i++) {// se inicia el for
              String num = String.valueOf(numero.charAt(i));// se crea la variable local num y con el metodo de String se pasa de char a string
@@ -35,9 +43,14 @@ public class Digitos
             numero = suma + "";// se pasa de int (que es el tipo de dato de suma) a string (que es el tipo de dato de número)
         }while(suma > 9);// se repite mientras la suma sea mayor a 9 es decir mientras hayan dos dígitos
     }
+     /**
+     
+     
+     *@return toString El método toString devuelve el resultado de la suma de lo dígitos del número
+     */
    public String toString()//inicia el método tipo String que muestra la suma 
     {
-        String cadena =  " ";// se declara la variable local cadena en blanco
+        String cadena =  " ";// se declara la variable local cadena en vacío
         cadena = "Suma: " + suma; // se le asigna a la cadena el valor de la suma
         return cadena;//se muestra el resultado de la suma
     }// fin del métdodo toString
